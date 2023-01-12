27a23240af64a1f24073fec2aee395a6a6cf2f81
Belen Balbis <git.hubalbis@gmail.com>
2023-01-12 17:16:00 -0300

    casi

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
-var lastMeetUpDate = new Date("Feb 05, 2023 15:26:25").getTime();
-var updateCountUpEverySecond = setInterval(function () {
-  var now = new Date().getTime();
-  var timeElapsed = lastMeetUpDate - now;
-  var days = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
-  var hours = Math.floor(
+let lastMeetUpDate = new Date("Feb 05, 2023 15:26:25").getTime();
+let updateCountUpEverySecond = setInterval(function () {
+  let now = new Date().getTime();
+  let timeElapsed = lastMeetUpDate - now;
+  let days = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
     (timeElapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
-  var minutes = Math.floor((timeElapsed % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000);
+  let minutes = Math.floor((timeElapsed % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000);
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
@@ -34,59 +34,98 @@ window.onload = function () {
 
 //SEGUNDA PRE ENTREGA
 //OBJETO
-const entrada1 = {
-  tipo: "VIP",
-  precio: 100,
-  ubicación: "Platea",
-};
+// const entrada1 = {
+//   tipo: "VIP",
+//   precio: 100,
+//   ubicación: "Platea",
+// };
 
-const entrada2 = {
-  tipo: "Campo",
-  precio: 80,
-  ubicación: "Campo",
-};
+// const entrada2 = {
+//   tipo: "Campo",
+//   precio: 80,
+//   ubicación: "Campo",
+// };
 
-const entrada3 = {
-  tipo: "General",
-  precio: 50,
-  ubicación: "Deriva",
-};
+// const entrada3 = {
+//   tipo: "General",
+//   precio: 50,
+//   ubicación: "Deriva",
+// };
 
-console.log(entrada1);
-console.log(entrada2);
-console.log(entrada3);
+// console.log(entrada1);
+// console.log(entrada2);
+// console.log(entrada3);
 
 //ARRAY
-const entradas = ["VIP", "Campo", "General"];
-alert("Recordá que nuestras entradas son " + entradas);
+// const entradas = ["VIP", "Campo", "General"];
+// alert("Recordá que nuestras entradas son " + entradas);
 
 //METODO
 //find()
 
-let entradaSeleccionada = prompt("Ingresá la entrada que querés comprar");
-alert(
-  "La entrada seleccionada es " +
-    entradas.find((entrada) => entrada === entradaSeleccionada)
-);
+// let entradaSeleccionada = prompt("Ingresá la entrada que querés comprar");
+// alert(
+//   "La entrada seleccionada es " +
+//     entradas.find((entrada) => entrada === entradaSeleccionada)
+// );
 
 //PRIMERA PRE ENTREGA
 //CICLO CON CONDICIONAL
-for (let i = 1; i <= 17; i++) {
-  let edad = prompt("Ingresá tu edad");
-  if (edad <= 17) {
-    alert("Te esperamos la próxima!");
-    break;
-  } else {
-    alert("Se viene lo bueno!");
+// for (let i = 1; i <= 17; i++) {
+//   let edad = prompt("Ingresá tu edad");
+//   if (edad <= 17) {
+//     alert("Te esperamos la próxima!");
+//     break;
+//   } else {
+//     alert("Se viene lo bueno!");
 
-    //FUNCTION PAGO TOTAL
-    function pagoTotal() {
-      const precio = 100;
-      let entradas = parseInt(prompt("Ingrese numero de entradas"));
-      let total = precio * entradas;
-      alert("El total a pagar es " + total + " U$S");
-    }
-    pagoTotal();
-    break;
-  }
+//     //FUNCTION PAGO TOTAL
+//     function pagoTotal() {
+//       const precio = 100;
+//       let entradas = parseInt(prompt("Ingrese numero de entradas"));
+//       let total = precio * entradas;
+//       alert("El total a pagar es " + total + " U$S");
+//     }
+//     pagoTotal();
+//     break;
+//   }
+// }
+
+//JSON Y STORAGE
+const botonColorMode = document.querySelector("#color-mode");
+const main = document.querySelector(".main");
+const navbar = document.querySelector(".navbar");
+const footer = document.querySelector(".footer");
+
+let darkMode = localStorage.getItem("dark-mode");
+
+function activarDarkMode() {
+  main.classList.add("dark-mode");
+  navbar.classList.add("dark-mode");
+  footer.classList.add("dark-mode");
+  localStorage.setItem("dark-mode", "activado");
+  botonColorMode.innerText = "DARK MODE";
 }
+
+function desactivarDarkMode() {
+  main.classList.remove("dark-mode");
+  navbar.classList.remove("dark-mode");
+  footer.classList.remove("dark-mode");
+  localStorage.setItem("dark-mode", "desactivado");
+  botonColorMode.innerText = "LIGHT MODE";
+}
+
+if (darkMode === "activado") {
+  activarDarkMode();
+} else {
+  desactivarDarkMode();
+}
+
+botonColorMode.addEventListener("click", () => {
+  darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "activado") {
+    desactivarDarkMode();
+  } else {
+    activarDarkMode();
+  }
+});
---
+++ secondary.js
@@ -0,0 +1,11 @@
+const botonModalCentro = document.querySelector("#boton-modal-centro");
+const modalCentro = document.querySelector("#modal-centro");
+const cerrarModalCentro = document.querySelector("#cerrar-modal-centro");
+
+botonModalCentro.addEventListener("click", () => {
+  modalCentro.classList.add("active");
+});
+
+cerrarModalCentro.addEventListener("click", () => {
+  modalCentro.classList.remove("active");
+});
